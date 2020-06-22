c2fb5bf81b2525cddfdeb034f5a11b1ec2e32bc4
James Robertson <james@jamesrobertson.eu>
2020-06-22 14:02:24 +0100

    feature: A wiki entry is now saved to XML as well as Markdown.

--- lib/wikisys.rb
+++ lib/wikisys.rb
@@ -3,10 +3,13 @@
 # file: wikisys.rb
 
 require 'dxlite'
+require 'martile'
 
 module Wikisys
 
   class Wiki
+    
+    attr_reader :to_xml
 
     def initialize(filepath='.', entries: 'entries.json', debug: false)
 
@@ -29,34 +32,122 @@ module Wikisys
     def page(title)
 
       r = @entries.find_by_title title   
-      @page = r ? File.read(title + '.md') : make_page(title)
+      @page = r ? read_md(title) : make_page(title)
+      @to_xml = build_xml @page
+      @entries.save
+      
+      return @page
 
     end
     
-    def page=(content)
+    def page=(raw_content)
       
-      title = content.lines.first.chomp
-      r = @entries.find_by_title title       
-      make_page(title) unless r
+      title = raw_content.lines.first.chomp
+      
+      r = @entries.find_by_title title
+      make_page(title, raw_content.lines.last.chomp[/(?<=\+ )/]) unless r
+                   
+      write_md title, raw_content
+      
+      @to_xml = build_xml raw_content      
+      write_xml title, @to_xml
 
-      File.write title + '.md', content
-      @page = content
+      
+      @entries.save
+      @page = raw_content
       
     end    
     
     private
     
-    def make_page(title)
+    def build_xml(content)
       
-      tags = title.downcase.gsub(/['\.\(\)]/,'').split.join(' ')
+      s = content.gsub(/\[\[[^\]]+\]\]/) do |raw_link|
+
+        title = raw_link[2..-3].strip
+        
+        r = @entries.find_by_title title            
+        
+        e = Rexle::Element.new('a').add_text title
+        e.attributes[:href] = title.gsub(/ +/, '_')
+        
+        if r then
+          
+          e.attributes[:title] = title.capitalize
+          
+        else
+          
+          make_page(title.capitalize)
+          e.attributes[:class] = 'new'
+          e.attributes[:title] = title.capitalize + ' (page does not exist)'
+          
+        end
+                
+        e.xml
+
+      end         
+      
+      
+      heading = "<heading>%s</heading>" % s.lines.first.chomp
+      tags = ''
+      
+      if s.lines.last[0] = '+' then
+      
+        list = s.lines.last[2..-1].split.map {|tag| "    <tag>%s</tag>" % tag}
+        tags = "<tags>\n%s\n  </tags>" % list.join("\n")
+        
+        body = "<body>\n    %s  </body>" % \
+            Martile.new(s.lines[1..-2].join.strip).to_html
+        
+      else
+        
+        body = "<body>%s</body>" % Martile.new(s.lines[1..-1].join.strip).to_html
+
+      end
+            
+      "<article>\n  %s\n  %s\n  %s\n</article>" % [heading, body, tags]
+      
+      
+    end
+    
+    def make_page(title, raw_tags=title.downcase.gsub(/['\.\(\)]/,''))
+      
+      tags = raw_tags.split.join(' ')
       s = "#{title}\n\n\n+ " + tags
-      File.write title + '.md', s
+      write_md title, s
+      write_xml title, build_xml(s)
       
       @entries.create title: title, tags: tags      
-      @entries.save
+      
       return s
       
     end
+    
+    def read_md(title)
+            
+      filepath = File.join(File.absolute_path(@filepath), 'md', 
+                           title.gsub(/ +/,'_') + '.md')      
+      File.read(title.gsub(/ +/,'_') + '.md')
+      
+    end
+    
+    def write_md(title, content)
+      
+      filepath = File.join(File.absolute_path(@filepath), 'md', 
+                           title.gsub(/ +/,'_') + '.md')
+      FileUtils.mkdir_p File.dirname(filepath)      
+      File.write filepath, content
+      
+    end
+    
+    def write_xml(title, content)
+      
+      filepath = File.join(File.absolute_path(@filepath), 'xml', 
+                           title.gsub(/ +/,'_') + '.xml')
+      FileUtils.mkdir_p File.dirname(filepath)      
+      File.write filepath, content
+      
+    end    
     
   end
 
--- wikisys.gemspec
+++ wikisys.gemspec
@@ -1,10 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'wikisys'
-  s.version = '0.1.1'
+  s.version = '0.2.0'
   s.summary = "A poor man's wiki."
   s.authors = ['James Robertson']
   s.files = Dir['lib/wikisys.rb']
-  s.add_runtime_dependency('dxlite', '~> 0.2', '>=0.2.1')
+  s.add_runtime_dependency('dxlite', '~> 0.2', '>=0.2.2')
+  s.add_runtime_dependency('martile', '~> 1.4', '>=1.4.3')
   s.signing_key = '../privatekeys/wikisys.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
