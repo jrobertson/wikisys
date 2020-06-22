d6ac53eb02e14404256a9ddeca175341f75e6378
James Robertson <james@jamesrobertson.eu>
2020-06-22 01:10:40 +0100

    1st commit

---
+++ lib/wikisys.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+
+# file: wikisys.rb
+
+require 'dxlite'
+
+module Wikisys
+
+  class Wiki
+
+    def initialize(filepath='.', entries: 'entries.json', debug: false)
+
+      @filepath = filepath
+      @page = ''
+      @debug = debug
+
+      @entries = if File.exists?(entries) then
+      
+        DxLite.new(entries)
+
+      else
+
+        DxLite.new('entries/entry(title, tags)', filepath: entries)
+
+      end
+    
+    end
+
+    def page(title)
+
+      r = @entries.find_by_title title   
+      @page = r ? r : make_page(title)
+
+    end
+    
+    def page=(content)
+      
+      title = content.lines.first.chomp
+      r = @entries.find_by_title title       
+      make_page(title) unless r
+
+      File.write title + '.md', content
+      @page = content
+      
+    end    
+    
+    private
+    
+    def make_page(title)
+      
+      tags = title.downcase.gsub(/['\.\(\)]/,'').split.join(' ')
+      s = "#{title}\n\n\n+ " + tags
+      File.write title + '.md', s
+      
+      @entries.create title: title, tags: tags      
+      @entries.save
+      return s
+      
+    end
+    
+  end
+
+end
+
+
---
+++ wikisys.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'wikisys'
+  s.version = '0.1.0'
+  s.summary = "A poor man's wiki."
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/wikisys.rb']
+  s.add_runtime_dependency('dxlite', '~> 0.2', '>=0.2.1')
+  s.signing_key = '../privatekeys/wikisys.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/wikisys'
+end
