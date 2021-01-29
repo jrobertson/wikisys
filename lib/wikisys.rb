1efc7f47818867616e377b7ea7ba1713220d81de
James Robertson <james@jamesrobertson.me.uk>
2021-01-29 17:58:23 +0000

    feature: Added the Pages class to allow wiki pages to be created from a Markdown file directory.

--- lib/wikisys.rb
+++ lib/wikisys.rb
@@ -3,12 +3,15 @@
 # file: wikisys.rb
 
 require 'dxlite'
+require 'dir-to-xml'
+require 'mindwords'
 require 'martile'
 
 module Wikisys
 
   class Wiki
     
+    attr_accessor :title, :content, :tags
     attr_reader :to_xml
 
     def initialize(filepath='.', entries: 'entries.json', debug: false)
@@ -17,17 +20,22 @@ module Wikisys
       @page = ''
       @debug = debug
 
-      @entries = if File.exists?(entries) then
+      @entries = if entries.is_a? DxLite then
+      
+        entries
+        
+      elsif File.exists?(entries) then
       
         DxLite.new(entries)
 
       else
 
-        DxLite.new('entries/entry(title, tags)', filepath: entries)
+        DxLite.new('entries/entry(title, tags)')
 
       end
     
     end
+    
 
     def page(title)
 
@@ -48,8 +56,8 @@ module Wikisys
       make_page(title, raw_content.lines.last.chomp[/(?<=\+ )/]) unless r
                    
       write_md title, raw_content
-      
-      @to_xml = build_xml raw_content      
+      title, content, tags = read_md()
+      @to_xml = build_xml title, content, tags
       write_xml title, @to_xml
 
       
@@ -58,13 +66,65 @@ module Wikisys
       
     end    
     
+    def modify_build(filename)
+      
+      @title, @content, @tags = read_md(filename)
+      
+      # find the entry
+      # modify the tags if necessary
+      
+      r = @entries.find_by_title @title
+      puts 'r: ' + r.inspect if @debug
+      return unless r
+      
+      write_xml(@title, build_xml(@title, @content, @tags))
+      
+      r.tags = @tags.join(' ') if r.tags != @tags
+      
+    end        
+    
+    def new_build(filename)
+      
+      @title, @content, @tags = read_md(filename)      
+      @entries.create title: @title, tags: @tags.join(' ')
+
+      puts 'md contents: ' + [@title, @content, @tags].inspect if @debug
+      write_xml(@title, build_xml(@title, @content, @tags))      
+      
+    end
+    
     private
     
-    def build_xml(content)
+    def read_md(filename)
       
-      s = content.gsub(/\[\[[^\]]+\]\]/) do |raw_link|
+      filepath = File.join(@filepath, 'md', filename)
+      return unless File.exists? filepath
+      
+      s = File.read(filepath).strip
+      
+      # read the title
+      title = s.lines.first.chomp.sub(/^# +/,'')
+      
+      # read the hashtags if there is any
+      tagsline = s.lines.last[/^ *\+ +(.*)/,1]
+      
+      if tagsline then
+        
+        [title, s.lines[1..-2].join, tagsline.split]
+      
+      else
+        
+        [title, s.lines[1..--1].join, []]
+        
+      end
+      
+    end                  
 
-        title = raw_link[2..-3].strip
+    
+    def build_xml(title, content, tags)
+      
+      puts 'content: ' + content.inspect if @debug
+      s = content.gsub(/\[\[[^\]]+\]\]/) do |raw_link|
         
         r = @entries.find_by_title title            
         
@@ -88,12 +148,11 @@ module Wikisys
       end         
       
       
-      heading = "<heading>%s</heading>" % s.lines.first.chomp
-      tags = ''
+      heading = "<heading>%s</heading>" % title
       
-      if s.lines.last[0] = '+' then
+      if tags.any? then
       
-        list = s.lines.last[2..-1].split.map {|tag| "    <tag>%s</tag>" % tag}
+        list = tags.map {|tag| "    <tag>%s</tag>" % tag}
         tags = "<tags>\n%s\n  </tags>" % list.join("\n")
         
         body = "<body>\n    %s  </body>" % \
@@ -110,6 +169,15 @@ module Wikisys
       
     end
     
+    def write_xml(title, content)
+      
+      filepath = File.join(File.absolute_path(@filepath), 'xml', 
+                           title.gsub(/ +/,'_') + '.xml')
+      FileUtils.mkdir_p File.dirname(filepath)      
+      File.write filepath, content
+      
+    end          
+    
     def make_page(title, raw_tags=title.downcase.gsub(/['\.\(\)]/,''))
       
       tags = raw_tags.split.join(' ')
@@ -121,13 +189,12 @@ module Wikisys
       
       return s
       
-    end
+    end          
     
-    def read_md(title)
-            
-      filepath = File.join(File.absolute_path(@filepath), 'md', 
-                           title.gsub(/ +/,'_') + '.md')      
-      File.read(title.gsub(/ +/,'_') + '.md')
+    def read_md_file(filename)
+      
+      filepath = File.join(@filepath, 'md', filename)
+      File.read(filepath)
       
     end
     
@@ -138,17 +205,124 @@ module Wikisys
       FileUtils.mkdir_p File.dirname(filepath)      
       File.write filepath, content
       
-    end
-    
-    def write_xml(title, content)
-      
-      filepath = File.join(File.absolute_path(@filepath), 'xml', 
-                           title.gsub(/ +/,'_') + '.xml')
-      FileUtils.mkdir_p File.dirname(filepath)      
-      File.write filepath, content
-      
-    end    
+    end      
     
   end
+  
+  class Pages
+    
+    attr_accessor :mw, :entries
 
+    def initialize(filepath='.', debug: false)
+      
+      @filepath, @debug = filepath, debug
+      
+      entries_file = File.join(@filepath, 'entries.txt')
+      
+      if File.exists?(entries_file) then
+        @entries = DxLite.new(entries_file)
+      else
+        @entries = DxLite.new('entries/entry(title, tags)')
+        @entries.save entries_file
+      end
+
+      # check for the mindwords raw document file
+      mindwords_file = File.join(@filepath, 'mindwords.txt')
+      
+      if File.exists?(mindwords_file) then 
+        @mw = MindWords.new(mindwords_file)
+      else
+        @mw = MindWords.new
+        @mw.filepath = mindwords_file
+      end
+      
+      scan_md_files()
+      
+    end
+    
+    private        
+
+    
+    # Check if any of the md files have been modified or newly created
+    #
+    def scan_md_files()
+      
+      filepath = File.join(@filepath, 'md')
+      puts 'about to scan ' + filepath.inspect if @debug
+      dir = DirToXML.new(filepath, index: 'dir.json', debug: @debug)
+      h = dir.activity
+      puts 'h: ' + h.inspect if @debug
+      
+      pg = Wiki.new @filepath, entries: @entries
+                    
+      
+      h[:new].each do |filename|
+
+        pg.new_build(filename)
+        update_mw(pg.title, pg.tags)
+        
+      end
+      
+      h[:modified].each do |filename|
+        
+        pg.modify_build(filename)
+        update_mw(pg.title, pg.tags)
+                
+      end
+      
+      @entries.save
+      @mw.save if @mw.lines.any?
+      
+    end # /scan_md_files       
+    
+    def update_mw(title, line_tags)
+      
+      # read the file
+
+      tags = line_tags.reject {|x| x =~ /#{title.strip}/i}
+      puts 'tags: '  + tags.inspect if @debug
+      puts 'title: ' + title.inspect if @debug
+      
+      return if tags.empty?
+      
+      line = title + ' ' + tags.map {|x| "#" + x }.join(' ') + "\n"
+      puts 'line: ' + line.inspect if @debug
+      
+      # does the tagsline contain the topic hashtag?
+      
+      #if tagsline =~ /#/ # not yet implemented
+        
+
+                            
+      # check if the title already exists
+      found = @mw.lines.grep(/^#{title} +(?=#)/i)
+      
+      if found.any? then
+        
+        found_tags = found.first.scan(/(?<=#)\w+/)
+        
+        if @debug then
+          
+          puts 'tags: ' + tags.inspect
+          puts 'found_tags: ' + found_tags.inspect
+        
+        end
+
+        new_tags = tags - found_tags   
+        
+        # add the new tags to the mindwords line
+        
+        hashtags = (found_tags + new_tags).map {|x| '#' + x }.join(' ')
+
+        i = @mw.lines.index(found.first)
+        @mw.lines[i] = line
+        
+      else
+        
+        @mw.lines <<  line
+        
+      end                            
+      
+    end
+  end
 end
--- wikisys.gemspec
+++ wikisys.gemspec
@@ -1,11 +1,12 @@
 Gem::Specification.new do |s|
   s.name = 'wikisys'
-  s.version = '0.2.0'
+  s.version = '0.3.0'
   s.summary = "A poor man's wiki."
   s.authors = ['James Robertson']
   s.files = Dir['lib/wikisys.rb']
-  s.add_runtime_dependency('dxlite', '~> 0.2', '>=0.2.2')
-  s.add_runtime_dependency('martile', '~> 1.4', '>=1.4.3')
+  s.add_runtime_dependency('dir-to-xml', '~> 1.0', '>=1.0.4')
+  s.add_runtime_dependency('mindwords', '~> 0.5', '>=0.5.3')
+  s.add_runtime_dependency('martile', '~> 1.4', '>=1.4.6')
   s.signing_key = '../privatekeys/wikisys.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
